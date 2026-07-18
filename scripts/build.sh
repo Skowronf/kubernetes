34ae8f39ce429ef6597f073e2f8ff2d5c0e6d52c
Skowronf <skowron.f.j@gmail.com>
2026-07-18 10:32:58 +0000

    Added ingress nginx

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,4 @@ mvn clean package -DskipTests
 docker build -t ghcr.io/skowronf/enterprise-platform:ci .
 docker push ghcr.io/skowronf/enterprise-platform:ci
 #trivy image petclinic:ci --severity CRITICAL
+#docker build -t petclinic:ci .
