504340fadeebdf1cad7b41d887aa059aa2e74520
Skowronf <skowron.f.j@gmail.com>
2026-07-18 13:23:56 +0000

    Fixing bug that prometheus stack was too big - CustomResourceDefinition.apiextensions.k8s.io "alertmanagerconfigs.monitoring.coreos.com" is invalid: metadata.annotations: Too long: may not be more than 262144 bytes,

--- scripts/argo.sh
+++ scripts/argo.sh
@@ -3,7 +3,7 @@ kubectl apply -n argocd \
   -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # before port forward let pods time to start running
 # get password
-#kubectl -n argocd get secret argocd-initial-admin-secret \
-#  -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d
 kubectl port-forward svc/argocd-server -n argocd 8090:443
 
