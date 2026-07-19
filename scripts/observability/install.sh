1c970d0203e2b60538d73c9bf1139ee982841a1f
Skowronf <skowron.f.j@gmail.com>
2026-07-19 09:46:09 +0000

    Added ArgoCD sync and health check for bootstrap and fixed e2e tests to run on ingress host

--- scripts/observability/install.sh
+++ scripts/observability/install.sh
@@ -29,3 +29,25 @@ kubectl -n argocd get secret argocd-initial-admin-secret \
 echo "Applying applications"
 
 kubectl apply -f gitops/applications/
+
+echo "Waiting for Argo Applications to appear (might want to add exit condition)"
+
+until kubectl get applications -n argocd --no-headers 2>/dev/null | grep -q .; do
+  sleep 2
+done
+
+echo "Waiting for applications to be sync with argo"
+
+kubectl wait \
+  -n argocd   \
+  --for=jsonpath='{.status.sync.status}'=Synced \
+  application --all \
+  --timeout=600s
+
+echo "Waiting for applications to be healthy in argo"
+
+kubectl wait \
+  -n argocd  \
+  --for=jsonpath='{.status.health.status}'=Healthy \
+  application --all \
+  --timeout=600s
