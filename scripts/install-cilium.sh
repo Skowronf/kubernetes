7449ec9d786ec8a1c4a5ad6e8966154eb6f25b50
Skowronf <skowron.f.j@gmail.com>
2026-07-24 16:24:57 +0000

    Made sure that Cilium taken control of netowrking instead of kube-proxy

--- scripts/install-cilium.sh
+++ scripts/install-cilium.sh
@@ -1,6 +1,8 @@
 source ./scripts/env.sh
 
-echo "Installing Cilium"
-cilium install
+echo "Installing Cilium" 
+cilium install --set kubeProxyReplacement=true --set k8sServiceHost=127.0.0.1 --set k8sServicePort=6443
 echo "Waiting for Cilium to be ready"
 cilium status --wait
+
+#kubectl apply -f policies/network/deny-all.yml
