2438bb1a7dd2c95e29c1c1b40876a9ee2787144c
Skowronf <skowron.f.j@gmail.com>
2026-07-28 10:47:57 +0000

    Added registry credentials to pull image and argo rollouts instead of deployment
    Deleted token and created new one locally
    
    Fixed brake line

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 ./scripts/update-docker-image.sh
 ./scripts/cluster.sh
 ./scripts/install-cilium.sh
+./scripts/create-registry-credentials.sh
 ./scripts/observability/install.sh
 ./scripts/smoke-test.sh
 ./scripts/e2e.sh
