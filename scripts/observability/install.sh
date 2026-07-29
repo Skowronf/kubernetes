324b1a56dfdf91e2535c2f0235339394ffe0d715
Skowronf <skowron.f.j@gmail.com>
2026-07-29 12:39:02 +0000

    Added rollback based on prometheus scraped data  - 500's

--- scripts/observability/install.sh
+++ scripts/observability/install.sh
@@ -42,7 +42,7 @@ kubectl wait \
   -n argocd   \
   --for=jsonpath='{.status.sync.status}'=Synced \
   application --all \
-  --timeout=800s
+  --timeout=1200s
 
 echo "Waiting for applications to be healthy in argo"
 
@@ -50,4 +50,4 @@ kubectl wait \
   -n argocd  \
   --for=jsonpath='{.status.health.status}'=Healthy \
   application --all \
-  --timeout=800s
+  --timeout=1200s
