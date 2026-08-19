f95d2645c13cafcf7d7cc0fd8f55eda51904e3a4
Skowronf <skowron.f.j@gmail.com>
2026-08-19 10:20:21 +0000

    Cleanup after dividing resposibilities per repo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./scripts/build.sh
-./scripts/update-docker-image.sh
+# ./scripts/build.sh
+# ./scripts/update-docker-image.sh
 ./scripts/cluster.sh
 ./scripts/install-cilium.sh
 ./scripts/create-registry-credentials.sh
 ./scripts/observability/install.sh
 ./scripts/smoke-test.sh
-./scripts/e2e.sh
+# ./scripts/e2e.sh
 
 echo "Done"
