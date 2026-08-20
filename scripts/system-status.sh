79d1124f87c66425d678c581896ac895dfef9d34
Skowronf <skowron.f.j@gmail.com>
2026-08-20 10:05:16 +0000

    added system status bash script

---
+++ scripts/system-status.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+CURRENT_USER=$(whoami)
+CORE_NUMBER=$(nproc)
+HOST_NAME=$(hostname)
+LINUX_KERNEL=$(uname -r)
+DATE=$(date)
+OS=$(uname -o)
+RAM=$(free -h | awk 'NR==2 {print $2}')
+echo "working directory: $PWD"
+echo "date: $DATE"
+echo "linux kernel: $LINUX_KERNEL"
+echo "operating system: $OS"
+echo "host name: $HOST_NAME"
+echo "current user: $CURRENT_USER"
+echo "number of cores: $CORE_NUMBER"
+echo "ram: $RAM"
