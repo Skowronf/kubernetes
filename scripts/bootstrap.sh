1ab6a1aa878d657139df504d769dd5914bb9f609
Skowronf <skowron.f.j@gmail.com>
2026-08-22 09:02:12 +0000

    deleted scripts for build and push image since app github actions workflow took over this resposibility

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ./scripts/build.sh
-# ./scripts/update-docker-image.sh
 ./scripts/cluster.sh
 ./scripts/install-cilium.sh
 ./scripts/create-registry-credentials.sh
