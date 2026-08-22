c4e8db45de486b1e4a30339774c9d72ac745042f
Skowronf <skowron.f.j@gmail.com>
2026-08-22 09:17:45 +0000

    refactored cluster script with better bash practicies

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -5,10 +5,11 @@ set -e
 CLUSTER_NAME="petclinic-ci"
 
 kind delete cluster \
-  --name ${CLUSTER_NAME} || true
+  --name "$CLUSTER_NAME" || true
 # |true:
-#   Ensures that the script continues even if the cluster does not exist.
+#   Ensures that the script continues even if the cluster delete command fails 
 
 kind create cluster \
-  --name ${CLUSTER_NAME} \
+  --name "$CLUSTER_NAME" \
   --config bootstrap/kind/cluster.yml
+# script must be run from the root of the repository, otherwise kind will not find the config file.
