1994c8ef51a0d89ae13d47a5122193f911a5da73
Skowronf <skowron.f.j@gmail.com>
2026-08-22 09:25:01 +0000

    refactored create local ca secret script with better bash practicies

--- scripts/create-local-ca-secret.sh
+++ scripts/create-local-ca-secret.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 
 echo "Creating local CA secret"
 
-CAROOT=$(mkcert -CAROOT)
+# Get the directory where mkcert stores the local CA certificate and private key.
+CAROOT="$(mkcert -CAROOT)"
 
 kubectl create secret tls local-ca-keypair \
   --namespace cert-manager \
   --cert="${CAROOT}/rootCA.pem" \
   --key="${CAROOT}/rootCA-key.pem" \
   --dry-run=client \
-  -o yaml | kubectl apply -f -
+  -o yaml | 
+  kubectl apply -f -
 
 echo "Local CA secret created"
